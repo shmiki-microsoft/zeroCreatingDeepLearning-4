a6784dcfbc91662faf0219cd991c79e15f51f1bd
Shunsuke Miki <miki.shunsuke@outlook.com>
2025-08-19 22:41:09 +0900

    modify

--- ch01/non_stationary.py
+++ ch01/non_stationary.py
@@ -16,3 +16,58 @@ class NonStatBandit:
         else:
             return 0
 
+class AlphaAgent:
+    def __init__(self, epsilon, alpha, actions=10):
+        self.epsilon = epsilon
+        self.Qs = np.zeros(actions)
+        self.alpha = alpha
+    
+    def update(self, action, reward):
+        self.Qs[action] += (reward - self.Qs[action]) * self.alpha
+
+    def get_action(self):
+        if np.random.rand() < self.epsilon:
+            return np.random.randint(0, len(self.Qs))
+        return np.argmax(self.Qs)
+
+runs = 200
+steps = 1000
+epsilon = 0.1
+alpha = 0.8
+agent_types = ['sample avarage','alpha const update']
+results = {}
+
+for agent_type in agent_types:
+    all_rates = np.zeros((runs, steps)) # (200,1000)
+
+    for run in range(runs):
+        if agent_type == 'sample avarage':
+            agent = Agent(epsilon)
+        else:
+            agent = AlphaAgent(epsilon, alpha)
+
+        bandit = NonStatBandit()
+        total_reward = 0
+        rates = []
+
+        for step in range(steps):
+            action = agent.get_action()
+            reward = bandit.play(action)
+            agent.update(action, reward)
+            total_reward += reward
+            rates.append(total_reward / (step + 1))
+
+        all_rates[run] = rates
+    
+    avg_rates = np.average(all_rates, axis=0)
+    results[agent_type] = avg_rates
+
+
+# plot
+plt.figure()
+plt.ylabel('Average Rates')
+plt.xlabel('Steps')
+for key, avg_rates in results.items():
+    plt.plot(avg_rates, label=key)
+plt.legend()
+plt.show()
