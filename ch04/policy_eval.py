a6baaafa3be8d49029a7e6628dbbe416d0c3651e
shmiki <shmiki@microsoft.com>
2025-10-22 09:30:16 +0900

    modify

---
+++ ch04/policy_eval.py
@@ -0,0 +1,18 @@
+import os, sys
+from collections import defaultdict
+from common.gridworld import GridWorld
+
+def eval_onestep(pi, V, env, gamma=0.9):
+    for state in env.states():
+        if state == env.goal_state:
+            V[state] = 0
+            continue
+
+        action_probs = pi[state]
+        new_V = 0
+        for action, action_prob in action_probs.items():
+            next_state = env.next_state(state, action)
+            r = env.reward(state, action, next_state)
+            new_V += action_prob * (r + gamma * V[next_state])
+        V[state] = new_V
+    return V
