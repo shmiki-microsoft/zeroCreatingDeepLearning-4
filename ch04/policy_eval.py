7bd4f468009d013906f5b74ea0c2fbfc065f4f5c
shmiki <miki.shunsuke@outlook.com>
2025-10-23 10:08:43 +0900

    modify

--- ch04/policy_eval.py
+++ ch04/policy_eval.py
@@ -16,3 +16,28 @@ def eval_onestep(pi, V, env, gamma=0.9):
             new_V += action_prob * (r + gamma * V[next_state])
         V[state] = new_V
     return V
+
+def policy_eval(pi, V, env, gamma, threshold=0.001):
+    while True:
+        old_V = V.copy()
+        V = eval_onestep(pi, V, env, gamma)
+
+        delta = 0
+        for state in V.keys():
+            t = abs(V[state]- old_V[state])
+            if delta < t:
+                delta = t
+
+        if delta < threshold:
+            break
+    return V
+
+if __name__ == '__main__':
+    env = GridWorld()
+    gamma = 0.9
+
+    pi = defaultdict(lambda: {0: 0.25, 1: 0.25, 2: 0.25, 3: 0.25})
+    V = defaultdict(lambda: 0)
+
+    V = policy_eval(pi, V, env, gamma)
+    env.render_v(V, pi)
