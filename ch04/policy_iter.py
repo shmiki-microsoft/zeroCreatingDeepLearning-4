f67b3fc7d1e013787dab2769d4ab3711a6b248f5
shmiki <shmiki@microsoft.com>
2025-10-28 13:50:24 +0900

    modify

--- ch04/policy_iter.py
+++ ch04/policy_iter.py
@@ -6,7 +6,7 @@ def argmax(d):
     """d (dict)"""
     max_value = max(d.values())
     max_key = -1
-    for key, value in d.item():
+    for key, value in d.items():
         if value == max_value:
             max_key = key
     return max_key
@@ -29,3 +29,23 @@ def greedy_policy(V, env, gamma):
         pi[state] = action_probs
     return pi
 
+def policy_iter(env, gamma, threshold=0.0001, is_render=True):
+    pi = defaultdict(lambda: {0:0.25, 1:0.25, 2:0.25, 3:0.25})
+    V = defaultdict(lambda: 0)
+
+    while True:
+        V = policy_eval(pi, V, env, gamma, threshold)
+        new_pi = greedy_policy(V, env, gamma)
+        if is_render:
+            env.render_v(V, pi)
+
+        if new_pi == pi:
+            break
+        pi = new_pi
+
+    return pi
+
+if __name__ == '__main__':
+    env = GridWorld()
+    gamma = 0.9
+    pi = policy_iter(env, gamma)
