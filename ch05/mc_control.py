4ab894d0da77a0c01c701cbeb68049dc67757fba
shmiki <shmiki@microsoft.com>
2025-11-11 13:48:39 +0900

    modify

---
+++ ch05/mc_control.py
@@ -0,0 +1,12 @@
+import numpy as np
+from collections import defaultdict
+from common.gridworld import GridWorld
+
+class McAgent:
+    def __init__(self):
+        self.gamma = 0.9
+        self.action_size = 4
+
+        random_actions = {0: 0.25, 1: 0.25, 2: 0.25, 3: 0.25}
+        self.pi = defaultdict(lambda: random_actions)
+        self.Q = defaultdict(lambda: 0)
