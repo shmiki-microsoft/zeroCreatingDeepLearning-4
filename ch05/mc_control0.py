c2a6806be51784ae46734a23cd5c78d3ce5d179e
shmiki <miki.shunsuke@outlook.com>
2025-11-12 14:58:28 +0900

    modify

--- ch05/mc_control0.py
+++ ch05/mc_control0.py
@@ -6,22 +6,20 @@ from common.gridworld import GridWorld
 def greedy_probs(Q, state, epsilon=0, action_size=4):
     qs = [Q[(state, action)] for action in range(action_size)]
     max_action = np.argmax(qs)
-    base_prob = epsilon / action_size
-    action_probs = {action: base_prob for action in range(action_size)} #{0: ε/4, 1: ε/4, 2: ε/4, 3: ε/4}
-    action_probs[max_action] += (1 - epsilon)
+    action_probs = {action: 0 for action in range(action_size)}
+    action_probs[max_action] = 1
     return action_probs
 
 class McAgent:
     def __init__(self):
         self.gamma = 0.9
-        self.epsilon = 0.1
-        self.alpha = 0.1
         self.action_size = 4
 
         random_actions = {0: 0.25, 1: 0.25, 2: 0.25, 3: 0.25}
         self.pi = defaultdict(lambda: random_actions)
         self.Q = defaultdict(lambda: 0)
         self.memory = []
+        self.cnts = defaultdict(lambda:0)
 
     def get_action(self, state):
         action_probs = self.pi[state]
@@ -42,14 +40,15 @@ class McAgent:
             state, action, reward = data
             G = self.gamma * G + reward
             key = (state, action)
-            self.Q[key] += (G - self.Q[key]) * self.alpha
-            self.pi[state] = greedy_probs(self.Q, state, self.epsilon)
+            self.cnts[key] += 1
+            self.Q[key] += (G - self.Q[key]) / self.cnts[key]
+            self.pi[state] = greedy_probs(self.Q, state, self.cnts[key])
 
 
 env = GridWorld()
 agent = McAgent()
 
-episodes = 10000
+episodes = 1000
 for episode in range(episodes):
     state = env.reset()
     agent.reset()
@@ -64,5 +63,4 @@ for episode in range(episodes):
             break
 
         state = next_state
-
 env.render_q(agent.Q)
