541780bec5000321df5b388c8bfddd9b7b382a73
shmiki <miki.shunsuke@outlook.com>
2025-11-25 22:57:56 +0900

    modify

---
+++ ch06/td_eval.py
@@ -0,0 +1,15 @@
+from collections import defaultdict
+import numpy as np
+from common.gridworld import GridWorld
+
+class TdAgent:
+    def __init__(self):
+        self.gamma = 0.9
+        self.alpha = 0.01
+        self.action_size = 4
+
+        random_actions = {0: 0.25, 1: 0.25, 2: 0.25, 3: 0.25}
+        self.pi = defaultdict(lambda: random_actions)
+        self.V = defaultdict(lambda: 0)
+
+    
