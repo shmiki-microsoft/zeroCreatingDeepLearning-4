6aa87ee69861e0e131a9765bb4c4ce976b6bdd02
shmiki <shmiki@microsoft.com>
2025-12-19 18:23:17 +0900

    modify

--- ch08/gym_play.py
+++ ch08/gym_play.py
@@ -2,13 +2,14 @@ import numpy as np
 import gymnasium as gym
 
 
-env = gym.make('CartPole-v0')
-state = env.reset()
+env = gym.make('CartPole-v1', render_mode="human")
+observation, info = env.reset()
 done = False
 
 while not done:
-    env.render()
+    # env.render()
     action = np.random.choice([0, 1])
+    # next_state, reward, done, info = env.step(action)
     '''
     nv.step の戻り値は以下
     (
