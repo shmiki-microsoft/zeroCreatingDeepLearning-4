e84982e6c4b51f7d3440f090915eac1362ad6376
shmiki <shmiki@microsoft.com>
2025-12-22 13:45:51 +0900

    modify

---
+++ ch08/replay_buffer.py
@@ -0,0 +1,26 @@
+from collections import deque
+import random
+import numpy as np
+import gymnasium as gym
+
+class ReplayBuffer:
+    def __init__(self, buffer_size, batch_size):
+        self.buffer = deque(maxlen=buffer_size)
+        self.batch_size = batch_size
+
+    def add(self, state, action, reward, next_state, done):
+        data = (state, action, reward, next_state, done)
+        self.buffer.append(data)
+
+    def __len__(self):
+        return len(self.buffer)
+
+    def get_batch(self):
+        data = random.sample(self.buffer, self.batch_size)
+
+        state = np.stack([x[0] for x in data])
+        action = np.array([x[1] for x in data])
+        reward = np.array([x[2] for x in data])
+        next_state = np.stack([x[3] for x in data])
+        done = np.array([x[4] for x in data]).astype(np.int32)
+        return state, action, reward, next_state, done
