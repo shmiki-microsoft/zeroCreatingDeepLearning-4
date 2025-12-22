04fb8e06fbfaf5845c686fc0ee6c4526cbec610f
shmiki <shmiki@microsoft.com>
2025-12-22 13:55:43 +0900

    modify

--- ch08/replay_buffer.py
+++ ch08/replay_buffer.py
@@ -24,3 +24,25 @@ class ReplayBuffer:
         next_state = np.stack([x[3] for x in data])
         done = np.array([x[4] for x in data]).astype(np.int32)
         return state, action, reward, next_state, done
+
+if __name__ == "__main__":
+    env = gym.make('CartPole-v1', render_mode="human")
+    replay_buffer = ReplayBuffer(buffer_size=10000, batch_size=32)
+
+    for episode in range(10):
+        state, info  = env.reset()
+        done = False
+
+        while not done:
+            action = 0
+            next_state, reward, terminated, truncated, info = env.step(action)
+            done = terminated or truncated
+            replay_buffer.add(state, action, reward, next_state, done)
+            state = next_state
+
+    state, action, reward, next_state, done = replay_buffer.get_batch()
+    print(state.shape)  # (32, 4)
+    print(action.shape)  # (32,)
+    print(reward.shape)  # (32,)
+    print(next_state.shape)  # (32, 4)
+    print(done.shape)  # (32,)
